b32727112d52ad3a96f5b10df6e08221261e5b68
Billy Vanegas <42476995+BillyClassTime@users.noreply.github.com>
2019-01-15 19:58:28 +0100

    Implementing questions about parallelism

---
+++ Complete/ImplementingParallel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+namespace _01_Complete
+{
+    public class MyParallel
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Starting");
+            var c = new Counter();
+            c.ProcessDirectory();
+           
+        }
+        class Counter
+        {
+            ConcurrentDictionary<string, int> _wordCounts = new ConcurrentDictionary<string, int>();
+            public Action<DirectoryInfo> ProcessDirectory()
+            {
+                return (dirInfo =>
+                {
+                    var files = dirInfo.GetFiles("*.cs").AsParallel<FileInfo>();
+                    files.ForAll<FileInfo>(
+                        fileInfo =>
+                        {
+                            var fileContent = File.ReadAllText(fileInfo.FullName);
+                            var sb = new StringBuilder();
+                            foreach (var val in fileContent)
+                            {
+                                sb.Append(char.IsLetter(val) ? val.ToString().ToLowerInvariant() : " ");
+                            }
+                            string[] wordInFile = sb.ToString().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (var word in wordInFile)
+                            {
+                                _wordCounts.AddOrUpdate(word, 1, (s, n) => n + 1);
+                                //int value;
+                                //if (_wordCounts.TryGetValue(word, out value))
+                                //{
+                                //    _wordCounts[word] = value++;
+                                //}
+                                //else
+                                //{
+                                //    _wordCounts[word] = 1;
+                                //}
+                                //var value = _wordCounts.GetOrAdd(word, 0);
+                                //_wordCounts[word] = value++;
+
+                                
+                            }
+                        });
+                    var diretories = dirInfo.GetDirectories().AsParallel<DirectoryInfo>();
+                    diretories.ForAll<DirectoryInfo>(ProcessDirectory());
+                });
+            }
+        }
+    }
+}
---
+++ Complete/PerformanceOperationString.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+namespace _01_Complete
+{
+    public class PerformaceOperationString
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Performance of concatenation");
+            var pr = new PerformaceOperationString();
+            var stopWatch = new Stopwatch();
+            string string1, string2, string3;
+            string1 = new string('A', 15000);
+            string2 = new string('a', 15000);
+            stopWatch.Start();
+            //string3 = String.Format("{0}{1}", string1, string2);
+            string3 = $"{string1}{string2}";
+            //string3 = $"{string1}{new string('a', 14999)}";
+            stopWatch.Stop();
+            //pr.ShowTimes(stopWatch, "string3 = String.Format(\"{0}{1}\", string1, string2)");
+            pr.ShowTimes(stopWatch, "string3 = $\"{string1}{string2}");
+            stopWatch.Start();
+            var result = pr.StringCompare(string1, string2, string3);
+            stopWatch.Stop();
+            Console.WriteLine((result ? true : false).ToString());
+            stopWatch.Start();
+            result = pr.StringCompare(string1, string2, string3, false);
+            stopWatch.Stop();
+            pr.ShowTimes(stopWatch, "Comparando con StringBuilder and ToUpper() ==");
+            //pr.ShowTimes(stopWatch, "Comparando con adding string");
+            Console.WriteLine((result ? true : false).ToString());
+            Console.ReadLine();
+        }
+        public bool StringCompare(string string1, string string2, string string3)
+        {
+            var concatStrings = new StringBuilder(string1);
+            concatStrings.Append(string2);
+            //var result = (concatStrings.ToString().ToUpper() == string3.ToUpper());
+            var result = concatStrings.ToString().Equals(string3, StringComparison.CurrentCultureIgnoreCase);
+            return result;
+        }
+        public bool StringCompare(string string1, string string2, string string3, bool other = true)
+        {
+            var concatStrings = new StringBuilder(string1); concatStrings.Append(string2);
+            //var concatStrings = string1 + string2;
+            var result = (concatStrings.ToString().ToUpper() == string3.ToUpper());
+            //var result = concatStrings.ToString().Equals(string3, StringComparison.CurrentCultureIgnoreCase);
+            return result;
+        }
+        public void ShowTimes(Stopwatch stopWatch, string message)
+        {
+            // Get the elapsed time as a TimeSpan value.
+            var ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            var elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000000000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds );
+            Console.WriteLine($"RunTime:{message}->{elapsedTime}");
+        }
+    }
+}
