789eaf523ba9fea3763db88680df67d8f5b34b9c
Billy Vanegas <42476995+BillyClassTime@users.noreply.github.com>
2019-01-16 20:49:49 +0100

    Tokens in diffrents Threads

---
+++ Complete/40-TokenInDifferentThreads.cs
@@ -0,0 +1,24 @@
+﻿using System; using System.Threading.Tasks; using System.Threading;
+namespace _01_Complete {
+    class myTokenInDifferentThreads {
+        public static void Main()   {
+            Console.WriteLine("Token in different threads");
+            var tokenSource = new CancellationTokenSource();
+            var task = Task.Factory.StartNew<int>(() => RunTimer(tokenSource.Token));
+            Console.WriteLine("Press [Enter] to stop the thimer.");
+            Console.ReadLine();
+            tokenSource.Cancel();
+            //tokenSource.Token.Register(() => tokenSource.Cancel());
+            //tokenSource.Dispose();
+            //tokenSource.IsCancellationRequested = true; //Property or indexer 'CancellationTokenSource.IsCancellationRequested' cannot be assigned to -- it is read only	
+            Console.WriteLine("Timer stopped at {0}", task.GetAwaiter().GetResult());
+            Console.ReadLine();
+        }
+        static int RunTimer(CancellationToken cancellationToken)        {
+            var time = 0;
+            while (!cancellationToken.IsCancellationRequested)
+                time++;
+            return time;
+        }
+    }
+}
